18c7a269f4d8066af439f93adfad9f084989ba9f
unknown <titosolano@impulsocr.com>
2021-12-10 11:04:27 -0600

    prueba 3

--- lextools.js
+++ lextools.js
@@ -1,4 +1,4 @@
-console.log("prueba2");
+console.log("prueba3");
 var input1 = 0;
 var input2 = 0;
 var input3 = 0;
@@ -72,7 +72,8 @@ function ValorCAbogados (input,input2,cuantia){
     if(cuantia==4){return getCAValue(input)+getCAValue(input2)};
 };
 function ValorRPapel (input,pliegos,cuantia){
-    function getRPapelValue (inputValue){       
+    function getRPapelValue (inputValue){  
+        console.log(inputValue);     
         if(inputValue<=25000){return 12.5};
         if(inputValue>25000){return 25};
         if(inputValue>75000){return 31.25};
